992921d61083c6ce279bcc49489bebb414f4690b
Gabriel <gabriel.benini2@gmail.com>
2025-05-30 19:45:25 -0300

    Configurando projeto e adicionando conexao com banco de dados

---
+++ src/main/java/com/gabriel/sistema_agendamentos_consultas_medicas/Application.java
@@ -0,0 +1,13 @@
+package com.gabriel.sistema_agendamentos_consultas_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/sistema_agendamentos_consultas_medicas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.sistema_agendamentos_consultas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
