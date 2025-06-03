189a8c4e79c444473e42387e6c5b198aca4e9777
Gabriel <gabriel.benini2@gmail.com>
2025-06-03 01:28:29 -0300

    Criado metodo na Repository para checar se existe consulta ja marcada com medico em determinada data e horario

--- src/main/java/com/gabriel/sistema_agendamentos_consultas_medicas/repository/ConsultaRepository.java
+++ src/main/java/com/gabriel/sistema_agendamentos_consultas_medicas/repository/ConsultaRepository.java
@@ -3,5 +3,11 @@ package com.gabriel.sistema_agendamentos_consultas_medicas.repository;
 import com.gabriel.sistema_agendamentos_consultas_medicas.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    boolean existsByMedicoIdAndDataAndHora(Long medicoId, LocalDate data, LocalTime hora);
+
 }
