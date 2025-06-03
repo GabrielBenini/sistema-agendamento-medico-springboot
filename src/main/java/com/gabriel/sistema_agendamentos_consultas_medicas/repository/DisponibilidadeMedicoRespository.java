ea8c67f1abc6330b0a5450bfc6d756b9b9cd4c9b
Gabriel <gabriel.benini2@gmail.com>
2025-06-03 01:29:45 -0300

    Criado metodo na repository para checar se o dia da semana e o horario se enquadram na disponibilidade do medico

--- src/main/java/com/gabriel/sistema_agendamentos_consultas_medicas/repository/DisponibilidadeMedicoRespository.java
+++ src/main/java/com/gabriel/sistema_agendamentos_consultas_medicas/repository/DisponibilidadeMedicoRespository.java
@@ -2,12 +2,16 @@ package com.gabriel.sistema_agendamentos_consultas_medicas.repository;
 
 import com.gabriel.sistema_agendamentos_consultas_medicas.model.DisponibilidadeMedico;
 import com.gabriel.sistema_agendamentos_consultas_medicas.model.dtos.DisponibilidadeMedicoResponseDTO;
+import com.gabriel.sistema_agendamentos_consultas_medicas.model.enumerates.DiaDaSemana;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DisponibilidadeMedicoRespository extends JpaRepository<DisponibilidadeMedico, Long> {
 
     List<DisponibilidadeMedico> findByMedicoId(Long medicoId);
+
+    Optional<DisponibilidadeMedico> findByMedicoIdAndDiaDaSemana(Long medicoId, DiaDaSemana diaDaSemana);
 
 }
